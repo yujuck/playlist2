f27b8d8fcd37b78ea705282f3710e691832e587e
yujuck <327ellen@naver.com>
2019-12-12 14:13:31 +0900

    12/12 mypage, 이미지 업로드, 토큰 정리

--- routes/mypage.js
+++ routes/mypage.js
@@ -4,9 +4,24 @@ var MeetPost = require('../models/index.js').MeetPost;
 var Category = require('../models/index.js').Category;
 var User = require('../models/index.js').User;
 
-// 글 작성 페이지 렌더링
+// 사용자 정보 페이지 렌더링
 router.get('/', function (req, res, next) {
   res.render('mypage');
+});
+
+// 좋아요 목록 페이지 렌더링
+router.get('/mypage-good', function (req, res, next) {
+  res.render('mypage-good');
+});
+
+// 즐겨찾기 목록 페이지 렌더링
+router.get('/mypage-favorite', function (req, res, next) {
+  res.render('mypage-favorite');
+});
+
+// 참여 목록 페이지 렌더링
+router.get('/mypage-participant', function (req, res, next) {
+  res.render('mypage-participant');
 });
 
 module.exports = router;
